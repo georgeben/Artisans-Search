c7c25119f490bab62552b788d6691bd7ed4f27d7
Somto Onyekwere <somtochionyekwere@gmail.com>
2018-11-25 01:39:04 +0100

    Feat: Add functions for encryption

--- models/admin-model.js
+++ models/admin-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs')
 const Schema = mongoose.Schema;
 
 mongoose.connect("mongodb://localhost//artisan-search");
@@ -10,4 +11,34 @@ const adminSchema = new Schema({
 })
 
 
-module.exports = mongoose.model('Admin', adminSchema);
+let Admin = module.exports = mongoose.model('Admin', adminSchema);
+
+
+
+//query e.g { email: req.body.email}
+module.exports.checkIfUserExists = function(query, callback) {
+    Admin.findOne(query, callback)    
+
+}
+
+
+// newAdmin would be an instance of the model eg admin = new Admin({
+    // email: req,body.email .....
+//})
+
+module.exports.createAdmin = function(newAdmin, callback) {
+    bcrypt.hash(newAdmin.password, null, null, function(err, result) {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save(callback);
+    })
+}
+
+
+module.exports.comparePassword = function(userEmail, hash, callback) {
+    bcrypt.compare(userPassword, hash, function(err, isMatch) {
+        if (err) throw err;
+        callback(null, isMatch)
+    })
+}
+
